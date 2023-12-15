4f4e372943f3ab6ad7375c05df1ac88c94fa2822
HenkaGs <henkkatus2@gmail.com>
2023-12-15 02:44:25 +0200

    Project finished

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Project 1: Shared shopping list
 
-Documentation:
+## Documentation:
 
 link to the site: https://henkags-wsdproject.onrender.com
 
-shopping-lists
+### Description:
+Website where you can add shopping lists with items and mark off items when collected. When list is done you can deactivate the list. Main page has statistics of activated + deactivated lists and collected + uncollected items. Application is deployed to [Shared Shopping Lists](https://henkags-wsdproject.onrender.com) site. If wanted, application can be runned locally with "docker-compose up" command in shopping-lists folder and opening up localhost link.
 
-Description:
-Website where you can add shopping lists with items and mark off items when collected. When list is done you can deactivate the list. Shopping-lists is deployed to https://henkags-wsdproject.onrender.com site. If wanted, application can be runned locally with "docker-compose up" command in shopping-lists folder and opening up localhost link.
-
+### Application tests
+Application tests were made by using playwright. Tests and instructions can be found from e2e-playwright/tests/hello-world.spec.js file.
--- e2e-playwright/tests/hello-world.spec.js
+++ e2e-playwright/tests/hello-world.spec.js
@@ -1,30 +1,29 @@
-// const { test, expect } = require("@playwright/test");
-
-// test("Server responds with the text 'Hello world!'", async ({ page }) => {
-//   const response = await page.goto("/");
-//   expect(await response.text()).toBe("Hello world!");
-// });
-
 const { test, expect } = require('@playwright/test');
 
-// 1. Test Main Page Load
-test('Main page loads with correct title', async ({ page }) => {
+// Here are function tests using playwright.
+// You can test application by first running docker-compose up in e2e-playwright/tests folder 
+// and opening up other terminal in same folder and running:
+// docker compose run --entrypoint=npx e2e-playwright playwright test; if ($?) { docker compose rm -sf }
+// Note: When running tests, first deactivate all "Test List" lists from http://localhost:7777/lists
+
+
+// 1. Test load main page.
+test('Load main page "Shopping lists"', async ({ page }) => {
   await page.goto('http://localhost:7777');
-  const title = await page.title();
-  expect(title).toBe('Shopping lists');
+  const title = await page.textContent('h1');
+  expect(title).toBe('Shared Shopping Lists');
 });
 
-// 2. Test Adding a Shopping List
-test('Can add a new shopping list', async ({ page }) => {
+// 2. Test adding new list "Test List".
+test('Add new shopping list "Test List"', async ({ page }) => {
   await page.goto('http://localhost:7777/lists');
   await page.fill('input[name="name"]', 'Test List');
   await page.click('input[type="submit"]');
-  const response = await page.waitForResponse(response => response.url().endsWith('/lists') && response.status() === 303);
-  expect(response.status()).toBe(303);
+  expect(await page.getByText('Test List')).toBeTruthy();
 });
 
-// 3. Test Viewing a Shopping List
-test('Can view a specific shopping list', async ({ page }) => {
+// 3. Test viewing "Test List".
+test('Can view "Test List"', async ({ page }) => {
   await page.goto('http://localhost:7777/lists');
   const listLink = await page.$('a[href^="/lists/"]');
   if (listLink) {
@@ -32,25 +31,34 @@ test('Can view a specific shopping list', async ({ page }) => {
     const title = await page.textContent('h1');
     expect(title).not.toBe('');
   } else {
-    throw new Error('No shopping list found to view');
+    throw new Error('List does not exist');
   }
 });
 
-// 4. Test Adding an Item to a Shopping List
-test('Can add an item to a shopping list', async ({ page }) => {
-  // Assuming there is already a list created
-  await page.goto('http://localhost:7777/lists/1'); // Replace '1' with an existing list id
+// 4. Test adding item to "Test List".
+test('Can add item to "Test List"', async ({ page }) => {
+  await page.goto('http://localhost:7777/lists');
+  await page.locator('a:text("Test List")').click();
   await page.fill('input[name="name"]', 'Test Item');
   await page.click('input[type="submit"]');
   const itemText = await page.textContent('li');
   expect(itemText).toContain('Test Item');
 });
 
-// 5. Test Marking an Item as Collected
-test('Can mark an item as collected', async ({ page }) => {
-  // Assuming there is an item in the list
-  await page.goto('http://localhost:7777/lists/1'); // Replace '1' with an existing list id
+// 5. Test marking "Test Item" collected from "Test List".
+test('Can mark item as collected from "Test List"', async ({ page }) => {
+  await page.goto('http://localhost:7777/lists');
+  await page.locator('a:text("Test List")').click();
   await page.click('input[value="Mark collected!"]');
-  const itemElement = await page.$('del');
+  const itemElement = await page.$('div');
   expect(itemElement).not.toBeNull();
+});
+
+// 6. Test deactivating list and check that "Test List" doesn't exist.
+test('Can deactivate list"', async ({ page }) => {
+  await page.goto('http://localhost:7777/lists');
+  expect(await page.getByText('Test List')).toBeTruthy();
+  await page.click('input[value="Deactivate list!"]');
+  expect(await page.isVisible("text='Test List'")).not.toBeTruthy()
+ 
 });
