d6c2eb233077a47b531270aa11f84d681382c180
HenkaGs <henkkatus2@gmail.com>
2023-12-16 17:10:52 +0200

    nolists

--- shopping-lists/controllers/listController.js
+++ shopping-lists/controllers/listController.js
@@ -22,7 +22,7 @@ const viewMainPage = async (request) => {
   const data = {
     numberOfLists,
     numberOfItems,
-    noListsYet: numberOfLists === 0,
+    hasLists: numberOfLists > 0,
   };
 
   return new Response(await renderFile("main.eta", data), responseDetails);
